c259c765ee09ce27cf69e0b8060bfa54316c618d
zboero <58707730+zboero@users.noreply.github.com>
2023-03-31 19:52:18 -0300

    Update Corr_funct.py
    Modification in the plot scripts...
    Definition of the xCorr function...
    Implementation of the bootstraps errors...

--- Corr_funct.py
+++ Corr_funct.py
@@ -61,14 +61,12 @@ import random
 cols_Auger = ['year', 'day', 'dec', 'RA', 'azimuth', 'weight']
 df_Auger = pd.read_table(data_Events_a8, skiprows=33, names= cols_Auger, sep="\s+", index_col=False)
 
-#df_gxs   = pd.read_table(data_2MRS, skiprows=10,\
-#                         names=['RAdeg', 'DECdeg', 'l', 'b', 'k_c', 'h_c', 'j_c', 'k_tc', 'h_tc', 'j_tc',\
-#                               'e_k', 'e_h', 'e_j', 'e_kt', 'e_ht', 'e_jt', 'e_bv', 'r_iso', 'r_ext',\
-#                               'b/a', 'flgs', 'type', 'ts', 'v', 'e_v', 'c'], sep="\s+",\
-#                         index_col=False)
+df_2MRS   = pd.read_table(data_2MRS, skiprows=10,\
+                         names=['RAdeg', 'DECdeg', 'l', 'b', 'k_c', 'h_c', 'j_c', 'k_tc', 'h_tc', 'j_tc',\
+                               'e_k', 'e_h', 'e_j', 'e_kt', 'e_ht', 'e_jt', 'e_bv', 'r_iso', 'r_ext',\
+                               'b/a', 'flgs', 'type', 'ts', 'v', 'e_v', 'c'], sep="\s+",\
+                         index_col=False)
 
-#cols_gxs = ['ID', 'RAdeg', 'DECdeg', 'cz', 'Ktmag_abs', 'l_deg', 'b_deg']
-#df_gxs = pd.read_table(data_2MRS_lowM, skiprows=1, names= cols_gxs, sep="\s+", index_col=False)
 cols_gxs = [ 'RAdeg', 'DECdeg', 'Kcmag', 'Hcmag', 'Jcmag', 'Ktmag', 'K_abs', 'type', 'cz',
              'JNAME', 'W1mag', 'W2mag', 'W3mag', 'class(1AGN,2SF,3Passive)']
 df_LVS         = pd.read_table(data_LVS, skiprows=1, names= cols_gxs, sep="\s+", index_col=False)
@@ -77,7 +75,11 @@ df_LVS_Passive = pd.read_table(data_LVS_Passive, skiprows=1, names= cols_gxs, se
 df_LVS_Faint   = pd.read_table(data_LVS_Faint, skiprows=1, names= cols_gxs, sep="\s+", index_col=False)
 df_LVS_Bright  = pd.read_table(data_LVS_Bright, skiprows=1, names= cols_gxs, sep="\s+", index_col=False)
     
-#df_gxs = df_LVS_Faint
+#df_gxs = df_2MRS
+#df_gxs = df_LVS_Bright
+#df_gxs = df_LVS_Faint    
+#df_gxs = df_LVS_SF
+#df_gxs = df_LVS_Passive
 df_gxs = pd.concat([df_LVS_Faint, df_LVS_Bright]).reset_index(drop=True)
 
 ##########################################################################################################
@@ -85,8 +87,7 @@ df_gxs = pd.concat([df_LVS_Faint, df_LVS_Bright]).reset_index(drop=True)
 ##########################################################################################################
 
 # Define the number of galaxies and the size of the map
-n_evnt = 50000
-#nside  = 64
+n_evnt = 80000
 
 # Generate a model distribution for the incoming UHECRs
 # 1) The random component
@@ -118,7 +119,7 @@ def rdm_sample(n_evnt):
     return df_rdm
     
 df_rdm_gxs = rdm_sample( n_evnt )
-df_rdm_gxs = df_rdm_gxs[ df_rdm_gxs['colat (rad)'] > np.deg2rad(45.01) ]
+df_rdm_gxs = df_rdm_gxs[ df_rdm_gxs['colat (rad)'] > np.deg2rad(45.0001) ]
 
 # Adjust the sample to have the number equal to the number of galaxies in our sample
 n_gxs   = len(df_gxs)                                            # Number of galaxies
@@ -127,6 +128,66 @@ id_rdm  = df_rdm_gxs.index                                       # This variable
 list_id = id_rdm.tolist()                                        # Here we transform it to a list for do iterations below...
 id_pick = random.sample( list_id, n_rdm )                        # We choose the n_rdm indices of uhe_events from the df_rdm sample...
 df_rdm_gxs  = df_rdm_gxs.loc[ id_pick ]                          # We choose those events from dr_rdm
+
+# plot of the rdm sample
+def sky_plot( th, phi, th_str, phi_str, coord_sys, title, output_file ):
+    ''' Produces a scatter map in Mollweide projection with the position
+    of events in the sky.
+        
+    Parameters
+    ----------
+    th, phi : numpy arrays
+        The angular coordinates on the sphere of the events: co-latitude and longitude
+        Units must be in radians.
+    th_str, phi_str : strings
+        The labels of the coordinates (for example, 'RA', 'Dec' or 'lon', 'lat')
+    coord_sys : str
+        The coordinate system under use: 'Galactic', 'Equatorial', etc
+    title : str
+        The title of the plot
+    output_file : str
+        The output file (.png in general)
+        
+    Returns
+    -------
+    The figure with the scatter plot in Mollweide projection.
+    '''
+
+    org =0                                                 # Origin of the map
+    projection ='mollweide'                                # 2D projection
+    x = np.remainder( phi + 2.0*np.pi - org, 2.0*np.pi)    # shift phi (RA) values
+    ind = x > np.pi
+    x[ind] -= 2.0*np.pi                                    # scale conversion to [-180, 180]
+    x = -x                                                 # reverse the scale: East to the left
+    y = 0.5*np.pi - th                                     # From colatitude to latitude
+    
+    tick_labels = np.array([150, 120, 90, 60, 30, 0, 330, 300, 270, 240, 210])
+    tick_labels = np.remainder( tick_labels + 360 + org, 360 )
+    
+    fig = plt.figure( figsize=(10, 5) )
+    ax = fig.add_subplot( 111, projection=projection )
+    ax.scatter( x, y, s=1.5 )
+    ax.set_xticklabels( tick_labels )                      # we add the scale on the x axis
+    ax.set_title( title )
+    ax.title.set_fontsize(15)
+    ax.set_xlabel( phi_str )
+    ax.xaxis.label.set_fontsize(12)
+    ax.set_ylabel( th_str )
+    ax.yaxis.label.set_fontsize(12)
+    ax.grid('True', linestyle='--')
+    ax.text( 5.5, -1.3 , coord_sys , fontsize=12)
+    plt.savefig( output_file )
+    #plt.savefig(graficos+'vMF_dist.png')
+
+
+th_rdm        = df_rdm_gxs['colat (rad)'].to_numpy()
+phi_rdm       = df_rdm_gxs['l (rad)'].to_numpy()
+th_str, phi_str = 'Dec (deg)', 'RA (deg)'
+coord_sys = 'Equatorial'
+title = 'Rdm'
+output_file = graficos+'skymap_rdm.png'
+sky_plot( th_rdm, phi_rdm, th_str, phi_str, coord_sys, title, output_file )
+
 
 # We now compute the correlation function between this map and the Auger map
 # Define the radial profile function
@@ -160,65 +221,83 @@ def radial_profile( th, phi, th_gxs, phi_gxs, bins ):
         
     return prof
 
-#bins = np.deg2rad( np.arange(5,91,5) )
-#th_uhe  = np.deg2rad( 90 - df_Ev8['dec'].to_numpy() )
-#phi_uhe = np.deg2rad( df_Ev8['RA'].to_numpy() )
-#th_rdm  = df_rdm['colat (rad)'].to_numpy()
-#phi_rdm = df_rdm['l (rad)'].to_numpy()
-#
-#prof_rdm = np.zeros( len(bins) - 1 )
-#for i in range( len(df_rdm) ):
-#    prof_rdm_i = radial_profile( th_rdm[i], phi_rdm[i], th_uhe, phi_uhe, bins )
-#    prof_rdm  += prof_rdm_i
 
-cols_df_CRs = { 'colat (rad)' : th_CRs,           # colatitude in radians
-                'l (rad)'     : phi_CRs           # longitude  in radians
-              }
-df_CRs = pd.DataFrame( data= cols_df_CRs )
-df_CRs = df_CRs[ df_CRs['colat (rad)'] > np.deg2rad(45.01) ]
+def xCorr( th_rdm, phi_rdm, th_gxs, phi_gxs, th_uhe_model, phi_uhe_model, bins ):
+    ''' Radial profile around the position of galaxies
+    Parameters
+    ----------
+    th_rdm : numpy array
+        co-latitude (0,pi) in radians for each point of the rdm sample.
+    phi_rdm : numpy array
+        longitude (0,2*pi) in radians for each point of the rdm sample.
+    th_gxs : numpy array
+        co-latitude (0,pi) in radians for each galaxy in the sample.
+    phi_gxs : numpy array
+        longitude (0,2*pi) in radians for each galaxy in the sample.
+    th_uhe_model : numpy array
+        co-latitude (0,pi) in radians for each galaxy in the 2nd sample.
+    phi_uhe_model : numpy array
+        longitude (0,2*pi) in radians for each galaxy in the 2nd sample.
+    bins : numpy array
+        the bins of the radial profile
 
-th_uhe_model  = df_CRs['colat (rad)'].to_numpy()
-phi_uhe_model = df_CRs['l (rad)'].to_numpy()
-th_rdm        = df_rdm_gxs['colat (rad)'].to_numpy()
-phi_rdm       = df_rdm_gxs['l (rad)'].to_numpy()
+    Returns
+    -------
+    A numpy array with the xCorrelation function
+    
+    '''
+    prof_rdm = np.zeros( len(bins) - 1 )
+    for i in range( len(th_rdm) ):
+        prof_rdm_i = radial_profile( th_rdm[i], phi_rdm[i], th_uhe_model, phi_uhe_model, bins )
+        prof_rdm  += prof_rdm_i
 
-bins = np.deg2rad( np.arange(5,91,5) )
-prof_rdm = np.zeros( len(bins) - 1 )
-for i in range( len(df_rdm_gxs) ):
-    prof_rdm_i = radial_profile( th_rdm[i], phi_rdm[i], th_uhe_model, phi_uhe_model, bins )
-    prof_rdm  += prof_rdm_i
+    prof_gxs = np.zeros( len(bins) - 1 )
+    for i in range( len(th_gxs) ):
+        prof_gxs_i = radial_profile( th_gxs[i], phi_gxs[i], th_uhe_model, phi_uhe_model, bins )
+        prof_gxs += prof_gxs_i
 
-# plot of the scatter plot
-th_str, phi_str = 'Dec (deg)', 'RA (deg)'
-title = 'Model of UHECRs incoming events'
-output_file = graficos+'skymap_model.png'
-sky_plot( th_uhe_model, phi_uhe_model, th_str, phi_str, coord_sys, title, output_file )
+    Corr_func = prof_gxs / prof_rdm - 1.0
+    
+    return Corr_func
 
 ##########################################################################################################
 # 1) We now compute the number of pairs (gxs/UHECRs) at a given radius
 ##########################################################################################################
 
-#bins = np.deg2rad( np.arange(5,91,5) )
-th_gxs  = np.deg2rad( 90.0 - df_gxs['DECdeg'].to_numpy() )
-phi_gxs = np.deg2rad( df_gxs['RAdeg'].to_numpy() )
+# We compute the cross-correlation function
+th_rdm        = df_rdm_gxs['colat (rad)'].to_numpy()
+phi_rdm       = df_rdm_gxs['l (rad)'].to_numpy()
+th_gxs        = np.deg2rad( 90.0 - df_gxs['DECdeg'].to_numpy() )
+phi_gxs       = np.deg2rad( df_gxs['RAdeg'].to_numpy() )
+th_uhe_model  = th_CRs
+phi_uhe_model = phi_CRs
+#th_uhe_model  = np.deg2rad( 90.0 - df_Auger['DECdeg'].to_numpy() )
+#phi_uhe_model = np.deg2rad( df_Auger['RAdeg'].to_numpy() )
 
-prof_gxs = np.zeros( len(bins) - 1 )
-for i in range( len(df_gxs) ):
-    prof_gxs_i = radial_profile( th_gxs[i], phi_gxs[i], th_uhe_model, phi_uhe_model, bins )
-    prof_gxs += prof_gxs_i
+bins = np.deg2rad( np.arange(5,91,5) )
+xCorr = xCorr(th_rdm, phi_rdm, th_gxs, phi_gxs, th_uhe_model, phi_uhe_model, bins)
 
-
-##########################################################################################################
-# 2) We now compute the cross-correlation function between (gxs/UHERCs)
-##########################################################################################################
-Corr_func = prof_gxs / prof_rdm - 1.0
+# We compute the bootstrap error..
+n_boots   = 20
+idx_boots = np.random.choice( len(th_gxs), size=( n_boots, len(th_gxs) ), replace=True )
+xCorr_list_boots = []#np.array([])
+for i in range( 0, n_boots ):
+    th_gxs  = th_gxs[ idx_boots[i] ]
+    phi_gxs = phi_gxs[ idx_boots[i] ]
+    xCorr_i = xCorr(th_rdm, phi_rdm, th_gxs, phi_gxs, th_uhe_model, phi_uhe_model , bins)
+    xCorr_list_boots.append( xCorr_i )
+    
+percentiles = np.percentile( xCorr_list_boots, np.array( [15.87,84.13] ), axis=0 )
+err_low_boot  = percentiles[0]
+err_high_boot = percentiles[1]
+err = 0.5 * ( err_high_boot - err_low_boot )
 
 
 # We plot the correlation function
-output_file = graficos+'xCross_B+F_vs_model.png'
+output_file = graficos+'xCross_SF+Passive_vs_model.png'
 plt.figure()
-#plt.fill_between( x_fx[0] * rad2deg, x_fx[1] - sigma, x_fx[1] + sigma, color='darkcyan', alpha=0.4, linestyle='solid' )
-plt.plot( np.rad2deg( bins[1:] ), Corr_func, color='darkcyan', label='Cross-Corr B+F', linestyle='solid' )
+#plt.fill_between( np.rad2deg( bins[1:] ), xCorr - err, xCorr + err, color='darkcyan', alpha=0.4, linestyle='solid' )
+plt.plot( np.rad2deg( bins[1:] ), xCorr, color='darkcyan', label='Cross-Corr SF+Passive', linestyle='solid' )
 #
 #plt.title ( 'Correlation function', loc='center', fontsize='x-large')
 plt.xlabel( 'Angle[deg]', fontsize='x-large')
